b4982cc71e72439653058b0f0807836c7a013a3e
urbaji <145362879+urbaji@users.noreply.github.com>
2024-11-28 21:56:37 +0500

    Add files via upload

---
+++ Using Typecasting, identification, e.t.c
@@ -0,0 +1,17 @@
+Number1 = 235.6251234
+Number1 = int(Number1)
+
+Changable_name = False
+Changable_name = str(Changable_name)
+
+print(a)
+print(Changable_name)
+
+print("Type of a is:", type(a))
+print("Type of Changable_name is:", type(Changable_name))
+
+
+user_number = input("Enter your value:")
+print("Type of number is:", type(user_number))
+user_number = float(user_number)
+print(user_number)
